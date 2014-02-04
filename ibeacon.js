2dd9c2a970d2b8e48dd36256def549e44cacdaa2
Fredrik Eldh <fredrik.eldh@evothings.com>
2014-02-04 15:28:25 +0100

    Second try. Scanning kinda works. ScanRecord is now properly passed to us, but decoding is broken.

--- ibeacon.js
+++ ibeacon.js
@@ -1,4 +1,42 @@
-var ble = evothings.ble;
+function b64ToUint6 (nChr) {
+
+  return nChr > 64 && nChr < 91 ?
+      nChr - 65
+    : nChr > 96 && nChr < 123 ?
+      nChr - 71
+    : nChr > 47 && nChr < 58 ?
+      nChr + 4
+    : nChr === 43 ?
+      62
+    : nChr === 47 ?
+      63
+    :
+      0;
+
+}
+
+function base64DecToArr (sBase64, nBlocksSize) {
+
+  var
+    sB64Enc = sBase64.replace(/[^A-Za-z0-9\+\/]/g, ""), nInLen = sB64Enc.length,
+    nOutLen = nBlocksSize ? Math.ceil((nInLen * 3 + 1 >> 2) / nBlocksSize) * nBlocksSize : nInLen * 3 + 1 >> 2, taBytes = new Uint8Array(nOutLen);
+
+  for (var nMod3, nMod4, nUint24 = 0, nOutIdx = 0, nInIdx = 0; nInIdx < nInLen; nInIdx++) {
+    nMod4 = nInIdx & 3;
+    nUint24 |= b64ToUint6(sB64Enc.charCodeAt(nInIdx)) << 18 - 6 * nMod4;
+    if (nMod4 === 3 || nInLen - nInIdx === 1) {
+      for (nMod3 = 0; nMod3 < 3 && nOutIdx < nOutLen; nMod3++, nOutIdx++) {
+        taBytes[nOutIdx] = nUint24 >>> (16 >>> nMod3 & 24) & 255;
+      }
+      nUint24 = 0;
+
+    }
+  }
+
+  return taBytes;
+}
+
+window.iBeacon = {
 
 /** Start scanning for beacons.
 * <p>Found devices and errors will be reported to the supplied callbacks.</p>
@@ -9,10 +47,20 @@ var ble = evothings.ble;
 * @param {scanCallback} win
 * @param {failCallback} fail
 */
-function startScan(region, win, fail) {
-	ble.startScan(function(device) {
-		if(device.scanRecord.length == 30) {
-			var beacon = parseScanRecord(device);
+startScan: function(region, win, fail) {
+	evothings.ble.startScan(function(device) {
+		var sr = base64DecToArr(device.scanRecord);
+		console.log("found device: srl " + sr.byteLength + "("+device.scanRecord.length+"), name: " + device.name);
+		console.log(Array.prototype.map.call(sr, function(n) {
+			var s = n.toString(16);
+			if(s.length == 1) {
+				s = '0'+s;
+			}
+			return s;
+		}).join(' '));
+		//console.log(device.scanRecord);
+		if(sr.byteLength == 30) {
+			var beacon = parseScanRecord(device, sr);
 			var filtered = (region && ((region.uuid && beacon.uuid != region.uuid) ||
 				(region.major && region.major != beacon.major) ||
 				(region.minor && region.minor != beacon.minor)));
@@ -22,12 +70,11 @@ function startScan(region, win, fail) {
 			win(beacon);
 		}
 	}, fail);
-}
+},
 
 // returns a Beacon
-function parseScanRecord(device) {
+parseScanRecord: function(device, sr) {
 	var b = device;
-	var sr = device.scanRecord;
 	b.region = {
 		uuid: sr[9,16],
 		major: sr[25,2],
@@ -41,13 +88,13 @@ function parseScanRecord(device) {
 	b.estimatedDistance = b.rssi / b.txPower;
 	b.state = 0;
 	return b;
-}
+},
 
 /** Describes an iBeacon region.
 * @typedef {Object} Region
 * @property {string} uuid - Formatted according to RFC 4122. Usually identifies the manufacturer of the beacon.
-* @property {number} major - Unsigned 16-bit integer. Usually identifies the location in which the beacon is installed.
-* @property {number} minor - Unsigned 16-bit integer. Usually identifies the beacon within a location.
+* @property {number} major - Unsigned 16-bit integer. Usually identifies a particular set of beacons.
+* @property {number} minor - Unsigned 16-bit integer. Usually identifies the beacon within a set.
 */
 
 /** This function is a parameter to startScan() and is called when a new device is discovered.
@@ -76,9 +123,9 @@ function parseScanRecord(device) {
 
 /** Stop scanning.
 */
-function stopScan() {
-	ble.stopScan();
-}
+stopScan: function() {
+	evothings.ble.stopScan();
+},
 
 /** Connect to a beacon.
 * <p>Will periodically update the device's RSSI and estimated distance, and report these values to the supplied callback.</p>
@@ -89,13 +136,13 @@ This is likely to happen often, as beacons move out of range, or back into range
 * @param {connectCallback} win
 * @param {failCallback} fail
 */
-function connect(beacon, win, fail) {
-	ble.connect(beacon.address, function(handle, state) {
+connect: function(beacon, win, fail) {
+	evothings.ble.connect(beacon.address, function(handle, state) {
 		beacon.state = state;
 		beacon._handle = handle
 		win(beacon);
 		function doRssi() {
-			ble.rssi(handle, rssiHandler, fail);
+			evothings.ble.rssi(handle, rssiHandler, fail);
 		}
 		function rssiHandler(rssi) {
 			beacon.rssi = rssi;
@@ -105,7 +152,7 @@ function connect(beacon, win, fail) {
 		}
 		doRssi();
 	}, fail);
-}
+},
 
 /** This function is a parameter to connect() and is called when a beacon's values are updated.
 * @callback connectCallback
@@ -117,6 +164,8 @@ function connect(beacon, win, fail) {
 /** Stop the connection to a beacon.
 * @param {Beacon} beacon
 */
-function disconnect(beacon) {
-	ble.close(beacon._handle);
-}
+disconnect: function(beacon) {
+	evothings.ble.close(beacon._handle);
+},
+
+};
